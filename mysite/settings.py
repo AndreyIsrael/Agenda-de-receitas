0545347ee2ca3db8a7be48d0c402df65228dd049
AndreyIsrael <andreyisrael2000@gmail.com>
2026-04-16 00:14:53 -0300

    feat: add login and registration

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'receitas'
+    'receitas',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+LOGIN_REDIRECT_URL= 'home'
+LOGOUT_REDIRECT_URL= 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- mysite/urls.py
+++ mysite/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('receitas.urls')),
+    path('members/',include('django.contrib.auth.urls')),
+    path('members/',include('members.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header= "Minha agenda de receitas"
